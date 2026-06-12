90f6db4dac8cd8f0b9bfb5afa3264e8908b52c8b
deliciousNesquik <byte.in4matic@gmail.com>
2026-06-13 01:12:27 +0500

    feat: изменить flyout фильтрации и вынести в UserControl
    - Переработано flyout фильтрации, кнопки для применения и сброса фильтрации закреплены внизу окна
    - Window.Resources вынесен в UserControl.Resources . Планируется вынести все конвертеры в отдельный файл для общей загрузки

---
+++ FirebirdTraceAnalyzer/UserControls/FilterFlyout.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace FirebirdTraceAnalyzer.UserControls;
+
+public partial class FilterFlyout : UserControl
+{
+    public FilterFlyout()
+    {
+        InitializeComponent();
+    }
+}
